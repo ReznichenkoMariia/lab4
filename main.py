c7477af4db4dc42ff4fa59ca71390ddd48e79e80
Mariia <ivanchenkomariia@knu.ua>
2026-03-02 15:22:46 +0200

    Call перевірка степінь двійки from main

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import utils
+
+test_numbers = [8, 16, 17, 0]
+
+for num in test_numbers:
+    if utils.is_power_of_two(num):
+        print(f"Число {num} є степенем двійки")
+    else:
+        print(f"Число {num} НЕ є степенем двійки")
 
 from utils import factorial
 
