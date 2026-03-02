3bd31c2d5544fb143df7926814896051ec33de2f
Mariia Reznichenko <reznichenko.masha@gmail.com>
2026-03-02 15:26:09 +0200

    Added sum of digits

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from utils import factorial
 if __name__ == "__main__":
     number = 5
     print(f"Факторіал числа {number} дорівнює {factorial(number)}")
+from utils import sum_digits
--- utils.py
+++ utils.py
@@ -5,3 +5,11 @@ def factorial(n):
     for i in range(2, n + 1):
         result *= i
     return result
+
+def sum_of_digits(n):
+    n = abs(n)
+    total = 0
+    while n > 0:
+        total += n % 10
+        n //= 10
+    return total
