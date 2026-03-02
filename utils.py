519d52a885ae3e1f7c8923fb46b6e0270454f378
Liza <lizarderkach2007@knu.ua>
2026-03-02 14:23:20 +0200

    add power of two

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+
+def is_power_of_two(n: int) -> bool:
+    return n > 0 and (n & (n - 1)) == 0
 def factorial(n):
     if n < 0:
         raise ValueError("Факторіал не визначений для від'ємних чисел")
