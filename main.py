e011e8ce9d0962efcdd1af18832b9a6250086cf5
Mariia <ivanchenkomariia@knu.ua>
2026-03-02 13:44:53 +0200

    Add gcd call to main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ a, b = 48, 18
 
 from utils import is_even
 print (f"Число 24 парне? {is_even(10)}")
+
+import utils 
+
+number = 17 
+if utils.is_prime(number):
+    print(f"Число {number} є простим")
+else:
+    print(f"Число {number} не є простим")
