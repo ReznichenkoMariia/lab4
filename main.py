5f969e528dc30d3e19887ed2a1eda58aa19ce9c1
Mariia <ivanchenkomariia@knu.ua>
2026-03-02 14:44:15 +0200

    Add call to main.py

--- main.py
+++ main.py
@@ -23,3 +23,10 @@ else:
 
     print(f"Число {number} не є простим")
 
+import utils
+
+number = 16
+if utils.is_power_of_two(number):
+    print(f"Число {number} є степенем двійки")
+else:
+    print(f"Число {number} не є степенем двійки")
